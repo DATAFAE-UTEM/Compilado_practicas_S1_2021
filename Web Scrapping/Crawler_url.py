37c7a410ed41d8941df3304a519d95b639e9a88a
MartinAguilarR <63862167+MartinAguilarR@users.noreply.github.com>
2021-08-03 14:26:19 +0200

    Create Crawler_url.py

---
+++ Web Scrapping/Crawler_url.py
@@ -0,0 +1,36 @@
+# Importaciones
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pandas as pd
+import csv
+
+# Set opciones Chrome
+chrome_options = Options()
+chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--window-size=1920x1080")
+
+# Google Chrome
+driver = webdriver.Chrome(options=chrome_options,
+                          executable_path="C:\\Program Files (x86)\\chromedriver.exe")
+
+def link(url):
+    # Navegacion
+    driver.get(url)
+
+    # Data
+    data = {}
+    df = pd.DataFrame(columns=['Links'])
+
+    # Definicion de elementos
+    ids = driver.find_elements_by_css_selector('.mime_doc') #Definición de elemento que contiene link
+
+    for ii in ids:
+        # definicion de referencia, dentro del elemento definidio
+        data['Links'] = ii.get_attribute('href')
+        df = df.append(data, ignore_index=True)
+        print(df)
+    # creacion de .csv
+    df.to_csv('nombre')
+
+# Insertar link a scrapear
+link('')
